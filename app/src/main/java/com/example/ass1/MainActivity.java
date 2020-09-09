d1b2a226958f9c9139634113e690a6c136eae5d7
Byron <b.lewis.design.development@gmail.com>
2020-09-09 10:46:26 +1000

    Objects Created
    Friend object added, Task Object added

--- app/src/main/java/com/example/ass1/MainActivity.java
+++ app/src/main/java/com/example/ass1/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Friend f = new Friend("Byron", "Lewis", "Male", 32, "51 Pelsart Ave Penrith");
+    Task t = new Task("Do assignment", "WSU");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
